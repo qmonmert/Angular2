a9ec1af485f8c2e16f4f2b035f6fbc8fa2e9f407
Quentin Monmert <q.monmert@proxiad.com>
2016-01-31 12:30:10 +0100

    CRUD on strava activities

--- app/app.strava.js
+++ app/app.strava.js
@@ -17,11 +17,13 @@ System.register(['angular2/core'], function(exports_1) {
             }],
         execute: function() {
             Activity = (function () {
-                function Activity(name, distance) {
+                function Activity(name, distance, completed) {
                     this.name = name;
                     this.distance = distance;
+                    this.completed = completed;
                     this.name = name;
                     this.distance = distance;
+                    this.completed = completed;
                 }
                 return Activity;
             })();
@@ -29,10 +31,27 @@ System.register(['angular2/core'], function(exports_1) {
             AppStrava = (function () {
                 function AppStrava() {
                     this.title = 'Strava Activities';
+                    this.newActivity = new Activity();
                     this.activities = new Array();
-                    this.activities.push(new Activity('SL : 1h50', 25));
-                    this.activities.push(new Activity('Footing', 11));
+                    this.activities.push(new Activity('25\' + 10x100m r=30" + 20\' ', 11, true));
+                    this.activities.push(new Activity('SL : 1h50', 25, true));
+                    this.activities.push(new Activity('Footing', 11, false));
                 }
+                AppStrava.prototype.addActivity = function (activity) {
+                    if (activity.name !== undefined && activity.name !== ''
+                        && activity.distance !== undefined && activity.distance !== '') {
+                        if (!isNaN(parseInt(activity.distance))) {
+                            this.activities.push(activity);
+                            this.newActivity = new Activity();
+                        }
+                    }
+                };
+                AppStrava.prototype.removeActivity = function (activity) {
+                    this.activities.splice(this.activities.indexOf(activity), 1);
+                };
+                AppStrava.prototype.setCompleted = function (activity) {
+                    activity.completed = !activity.completed;
+                };
                 AppStrava = __decorate([
                     core_1.Component({
                         selector: 'strava',
--- app/app.strava.ts
+++ app/app.strava.ts
@@ -1,9 +1,10 @@
 import {Component, View} from 'angular2/core';
 
 export class Activity {
-    constructor(public name: string, public distance: number) {
+    constructor(public name?: string, public distance?: number, public completed?: boolean) {
         this.name = name;
-        this.distance = distance;    
+        this.distance = distance;  
+        this.completed = completed;  
     }
 }
 
@@ -11,24 +12,42 @@ export class Activity {
     selector: 'strava',
     templateUrl: 'app/views/strava.html',
     directives: []
-    /*
-    template:`
-        <div *ngFor="#activity of activities">
-            {{activity.name}} Q<br><br>
-        </div>
-        `
-     */
+    /* template:`
+        ...
+        ` */
 })
 export class AppStrava {
      
     public title = 'Strava Activities';
     
+    public newActivity: Activity;
+    
     activities: Array<Activity>;
     
     constructor() {
+        this.newActivity = new Activity();
         this.activities = new Array<Activity>();
-        this.activities.push(new Activity('SL : 1h50', 25));
-        this.activities.push(new Activity('Footing', 11));
+        this.activities.push(new Activity('25\' + 10x100m r=30" + 20\' ', 11, true));
+        this.activities.push(new Activity('SL : 1h50', 25, true));
+        this.activities.push(new Activity('Footing', 11, false));
+    }
+    
+    addActivity(activity) {
+        if (activity.name !== undefined && activity.name !== ''
+            && activity.distance !== undefined && activity.distance !== '') {
+            if (!isNaN(parseInt(activity.distance))) {  
+                this.activities.push(activity);
+                this.newActivity = new Activity();
+            }
+        }
+    }
+    
+    removeActivity(activity) {
+        this.activities.splice(this.activities.indexOf(activity), 1);
+    }
+    
+    setCompleted(activity) {
+        activity.completed = !activity.completed;
     }
     
 }
